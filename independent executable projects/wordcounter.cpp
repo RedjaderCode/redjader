e7a6d8d6bb419bf39990480c7abcd5278cdee9e3
Joshua Luijkx <108335919+RedjaderCode@users.noreply.github.com>
2022-11-06 17:08:05 +0100

    Add files via upload

--- independent executable projects/wordcounter.cpp
+++ independent executable projects/wordcounter.cpp
@@ -1,5 +1,9 @@
-//this program can count words in files to a fair accuracy.
-//Works best with just paragraphs, and the alike.
+/*
+this program can count words in files to a fair accuracy.
+Works best with just paragraphs, and the alike.
+
+For this program, put any file(s) inside the FILES director in order for it to work.
+*/
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -16,25 +20,28 @@ struct file{
 
     inline int wordcounter(){
         VIF.seekg(0, VIF.end);
-        size_t SizeOfFile = VIF.tellg();
-        char * buffer = new char[SizeOfFile];
-        int WordCountIterator=0;
+        size_t SizeOfFile        = VIF.tellg();
+        char * buffer            = new char[SizeOfFile];
+        int    WordCountIterator = 0;
+        
         for(int i=0; i<=SizeOfFile; ++i){
             VIF.seekg(i);
-            VIF.read((char*)&(*(buffer+i)), sizeof(char));
+            VIF.read ((char*)&(*(buffer+i)), sizeof(char));
+
             if(*(buffer+i)==' '&&*(buffer+i)!=*(buffer+(i+1))) ++WordCountIterator;
         }
         buffer[SizeOfFile] = '\0';
         if(*(buffer+(SizeOfFile))!=' ') ++WordCountIterator;
+        
         std::cout << buffer << std::endl;
         std::cout << "\twords: ";
-        return WordCountIterator - 1;
         delete[] buffer;
+        return WordCountIterator - 1;
     }
 };
 
 auto GetFileCount = std::filesystem::directory_iterator("FILES");
-int iterator;
+int  iterator;
 std::string assetsPaths[10];
 
 void CreateImgIterator(){
@@ -49,8 +56,10 @@ void CreateImgIterator(){
 
 int main(int s, char * z[]){
     CreateImgIterator();
+
     int choice; std::cout << "Choose file: ";
     std::cin >> choice; std::cout << std::endl;
+
     file FILE(assetsPaths[choice].c_str());
     std::cout << FILE.wordcounter() << std::endl;
     return 0;
