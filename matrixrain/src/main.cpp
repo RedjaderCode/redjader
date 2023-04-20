541653e9d702673c02ca01839126a68c160f76ef
Joshua Luijkx <108335919+RedjaderCode@users.noreply.github.com>
2023-04-20 13:27:07 +0200

    Add files via upload

--- matrixrain/src/main.cpp
+++ matrixrain/src/main.cpp
@@ -14,13 +14,27 @@ int main(int ss, char * zz[]){
     consolebuffer.ConstructConsole();
     consolebuffer.FlushScreenBuffer(L'|', dwBytesWritten);
 
+    /*
+    sStreamer s[ consolebuffer.nScreenWidth ];
+    for(uint16_t i=0; i <= consolebuffer.nScreenWidth; ++i)
+    {
+        uint16_t speed = rand() % 8 + 4;
+        size_t length  = rand() % 9 + 7;
+        s[ i ] = {speed, length};
+    }
+    */
+
     uint16_t speed = rand() % 8 + 4;
     size_t length  = rand() % 9 + 7;
     sStreamer s = {speed, length};
 
     while(running)
     {
-        Sleep(consolebuffer.WriteStreamToScreenBuffer(pPosition, dwBytesWritten, s.speed, s.length));
+        //uint16_t streamer = rand() % consolebuffer.nScreenWidth;
+
+        Sleep(consolebuffer.WriteStreamToScreenBuffer(
+            pPosition, dwBytesWritten, 
+            s/*[ streamer ]*/.speed, s/*[ streamer ]*/.length));
         pPosition.X = rand() % consolebuffer.nScreenWidth;
         
         if(GetAsyncKeyState(VK_ESCAPE & 0x1B)) running = false;
